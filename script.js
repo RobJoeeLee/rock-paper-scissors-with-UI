01b183b27cbb17e23bfd4894877fb19445537224
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2024-05-30 14:45:10 -0400

    setting up the intial game in the console.

---
+++ script.js
@@ -0,0 +1,39 @@
+function getComputerChoice(){
+    const choices = ["rock", "paper", "scissors"];
+    const randomNumber = Math.floor(Math.random() * choices.length);
+    return choices[randomNumber];
+}
+
+function getHumanChoice(){
+    let userInput = prompt("Enter your choice (rock, paper, or scissors):").toLowerCase();
+    while (userInput !== "rock" && userInput !== "paper" && userInput !== "scissors"){
+        userInput = prompt ("Invalid choice! Please enter either rock, paper, or scissors:").toLowerCase();
+    }
+    return userInput;
+}
+
+humanScore = 0;
+computeScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!")
+    } else if(
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        console.log("You Win! " + humanChoice + " beats " + computerChoice);
+        humanScore++;
+    } else {
+        console.log("You lose! " + computerChoice + " beats " + humanChoice);
+        computeScore++;
+    }
+}
+
+const computerChoice = getComputerChoice();
+const humanChoice = getHumanChoice();
+playRound(humanChoice, computerChoice);
